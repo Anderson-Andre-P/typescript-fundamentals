4f7cf72cfa51a0b4452658d0b1727b4472f7ca21
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2021-12-02 16:38:10 -0300

    feat: type-assertions-2

--- src/type-assertions.ts
+++ src/type-assertions.ts
@@ -1,16 +1,16 @@
 // Condicional (Recomendável)
-const body1 = document.querySelector('body');
-if (body1) body1.style.background = 'red';
+const body10 = document.querySelector('body');
+if (body10) body10.style.background = 'red';
 
-// Non-null assertion (!)  (Não recomendável)
-const body2 = document.querySelector('body')!;
-body2.style.background = 'red';
+// Non-null a0ssertion (!)  (Não recomendável)
+const body20 = document.querySelector('body')!;
+body20.style.background = 'red';
 
 // Type assertion (Recomendável)
-const body3 = document.querySelector('body') as HTMLBodyElement;
+const body30 = document.querySelector('body') as HTMLBodyElement;
 body3.style.background = 'red';
 
 // HTMLElement (Recomendável)
-const input = document.querySelector('.input') as HTMLInputElement;
-input.value = 'Any thing';
-input.focus();
+const input0 = document.querySelector('.input') as HTMLInputElement;
+input0.value = 'Any thing';
+input0.focus();
